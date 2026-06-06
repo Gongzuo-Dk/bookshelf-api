5cef468c6562a973516abfeaba592c586ca44aa8
Gongzuo-Dk <gongzuo.dk@gmail.com>
2026-06-06 18:14:42 +0300

    Change installed apps order

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'django.contrib.sites',
+    'dj_rest_auth.registration',
     'rest_framework.authtoken',
     'dj_rest_auth',
     'allauth',
@@ -35,7 +37,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
 
-    'rest_framework',
     'bookshelf',
     'accounts',
 ]
