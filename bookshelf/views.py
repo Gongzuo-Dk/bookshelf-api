3cdf1fb34f1e2588ba61d7383c686ba2179700b0
Gongzuo-Dk <gongzuo.dk@gmail.com>
2026-06-08 22:41:10 +0300

    Add ViewSet with ownership filtering and CRUD endpoints

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from rest_framework.exceptions import PermissionDenied
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book 
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["status", "genre"]
+    search_fields = ["title", "author"]
+    ordering_fields = ["rating", "created_at", "finish_date"]
+
+    def get_queryset(self):
+        return Book.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+
